2e5d3fba56609dce9d766f8c1a5cec7c1de4dafc
fadilkun45 <indocrafter76@gmail.com>
2021-03-03 15:17:10 +0700

    update

--- FRONT-END/js/home.js
+++ FRONT-END/js/home.js
@@ -1,39 +1,39 @@
-window.onload = loadhome()
+window.onload = starthome()
 
-function loadhome (){
+function starthome (){
   $(".container").load("section/home.html");
-    loadTomcat()
-    loadSenmed()
+    TomcatCaller()
+    SenmedCaller()
 }
 
-function loadTomcat(){
+function TomcatCaller (){
   fetch('https://api.mikasa-server.online/api/v1/youtube/?channel=tstream')
   .then(response => response.json())
   .then((data) => {
-    console.log(data['tstream'][0]['title'])
-    for(i = 0 ; i < 3 ; i++ ){
+  console.log(data['tstream'][0]['title'])
+  for(i = 0 ; i < 3 ; i++ ){
       const url = data['tstream'][i]['url'];
       const title = data['tstream'][i]['title'];
       const publish = data['tstream'][i]['publish'];
       const thumbnail = data['tstream'][i]['thumbnail'];
-      const container = document.querySelector(".card-trending");
+      const container = document.querySelector(".card-tomcat");
 
       container.innerHTML += `<div class="col-md text-center mb-3">
-      <div class="card">
-        <a href="` + url + `" class="no-hover">
-          <img class="card-img-top" src="` + thumbnail + `">
-          <div class="card-body">
-            <h5 class="card-title hapus-overflow text-title">` + title + `</h5>
-            <h6 class="card-subtitle mb-2 text-muted hapus-overflow text-subtitle">`+ publish +`</h6>
-          </div>
-        </a>
-      </div>
-    </div>`
+            <div class="card">
+              <a href="${url}" class="no-hover">
+                <img class="card-img-top" src="${thumbnail}">
+                <div class="card-body">
+                  <h5 class="card-title hapus-overflow text-title">${title}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted hapus-overflow text-subtitle">${publish}</h6>
+                </div>
+              </a>
+            </div>
+        </div>`
     }
   })
 }
 
-function loadSenmed (){
+function SenmedCaller (){
   fetch('https://api.mikasa-server.online/api/v1/youtube/?channel=senmed')
   .then(response => response.json())
   .then((data) => {
@@ -47,16 +47,16 @@ function loadSenmed (){
       const container = document.querySelector(".card-trending");
 
       container.innerHTML += `<div class="col-md text-center mb-3">
-      <div class="card">
-        <a href="` + url + `" class="no-hover">
-          <img class="card-img-top" src="` + thumbnail + `">
-          <div class="card-body">
-            <h5 class="card-title hapus-overflow text-title">` + title + `</h5>
-            <h6 class="card-subtitle mb-2 text-muted hapus-overflow text-subtitle">`+ publish +`</h6>
-          </div>
-        </a>
-      </div>
-    </div>`
+            <div class="card">
+              <a href="${url}" class="no-hover">
+                <img class="card-img-top" src="${thumbnail}">
+                <div class="card-body">
+                  <h5 class="card-title hapus-overflow text-title">${title}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted hapus-overflow text-subtitle">${publish}</h6>
+                </div>
+              </a>
+            </div>
+        </div>`
     }
   })
 }
--- FRONT-END/js/navbar-sidebar.js
+++ FRONT-END/js/navbar-sidebar.js
@@ -3,11 +3,77 @@ const sidebar = document.querySelector('.sidebar');
 const MainContainer = document.querySelector('.main-container');
 const container = document.querySelector('.container');
 const SearchBar = document.querySelector('.search-bar');
+const searchbarResult = document.querySelector('#livesearch');
+const SearchBarvalue = document.querySelector('#bar');
+
+// livesearch 
+SearchBarvalue.addEventListener('keyup', function showResult() {
+    fetch('https://api.mikasa-server.online/api/v1/youtube/?title='+ SearchBarvalue.value)
+      .then(response => response.json())
+      .then((data) => {
+          console.log(data['title'])
+          const title = data['title'];
+
+        if(title === undefined ){
+            searchbarResult.innerHTML=`<p>404 not found</p>`;
+        }else{
+            searchbarResult.innerHTML=`<p>${title}</p>`;
+        }
+        })
+})
+
+SearchBarvalue.addEventListener('blur', function () {
+    searchbarResult.innerHTML = '';
+    SearchBarvalue.value = '' ;
+
+})
+
+SearchBarvalue.addEventListener('keyup',function(event){
+    if(event.keyCode == 13) {
+        $(".container").load("section/search.html");
+        fetch('https://api.mikasa-server.online/api/v1/youtube/?title='+SearchBarvalue.value)
+        .then(response => response.json())
+        .then((data) => {
+            for(i = 0 ; i < data.length ; i++ ){
+                console.log(data[i])
+                const title = data[i][0];
+                const url = data[i][1];
+                const thumbnail = data[i][2];
+                const container = document.querySelector('.result');
+                container.innerHTML += `<div class="col-md text-center mb-3">
+                <div class="card">
+                  <a href="#" data-url="${url}" class="no-hover video-result">
+                    <img class="card-img-top" src="${thumbnail}">
+                    <div class="card-body">
+                      <h5 class="card-title hapus-overflow text-title">${title}</h5>
+                    </div>
+                  </a>
+                </div>
+            </div>`
+
+            $('.video-result').on('click',function (){
+                $(".container").load("section/stream.html");
+                const source = document.createElement('source');
+                const linkembed = "https://www.youtube.com/embed/"
+                const videoLink = linkembed + this.dataset.url ;
+                console.log(videoLink)
+                const layar = document.querySelector('#stream-player');
+                source.setAttribute('src',videoLink);
+                layar.appendChild(source);
+            })
+        }
+
+           
+            
+        })
+    };
+})
 
 
 // section caller
 
 function loadhome (){
+    starthome ()
     $(".container").load("section/home.html");
     checkedRemove()
 }   
@@ -32,6 +98,7 @@ function loadTrending (){
     checkedRemove()
 }
 
+
 function checkedRemove (){
     $(".rmvcheck").click(function(){
         $(toggle).prop("checked", false);
@@ -42,7 +109,7 @@ function checkedRemove (){
     MainContainer.classList.remove('container-utama-pengecil');
 }
 
-
+// sidebar
 toggle.addEventListener('change', function(event){
     if(this.checked){
         sidebar.classList.add('sidebar-hilang');
@@ -53,6 +120,7 @@ toggle.addEventListener('change', function(event){
     }
 })
 
+// script yang aktif ketika mobile
 if (document.documentElement.clientWidth < 800 ){
     toggle.addEventListener('change', function(event){
         if(this.checked){
